ed728d8047dcc1e4d22b3e533572dc0e7140b48d
Hari Hendaryanto <hari.hendaryanto@gmail.com>
2018-02-02 23:54:20 +0700

    Fix bug and add some remote keyw

--- remote.sh
+++ remote.sh
@@ -3,7 +3,7 @@ stty -F /dev/ttyACM0 9600 cs8 -cstopb -parenb -icanon min 1 time 1
 state="false"
 while true; do
         read -n 8 LINE < /dev/ttyACM0
-        LINE=$(echo -n $LINE | sed 's/[ \t\r\n]*//g')
+        LINE=$(echo -n $LINE | sed 's/[ \t\r\n]+//g')
         echo $LINE
         # PREV
         if [[ "$LINE" == "FF22DD" ]]
@@ -49,55 +49,5 @@ while true; do
         then
            mpc stop
            state="false"
-        fi
-        # Play #1
-        if [[ "$LINE" == "FF30CF" ]]
-        then
-           mpc play 1
-        fi
-        # Play #2
-        if [[ "$LINE" == "FF18E7" ]]
-        then
-           mpc play 2
-        fi
-        # Play #3
-        if [[ "$LINE" == "FF7A85" ]]
-        then
-           mpc play 3
-        fi
-        # Play #4
-        if [[ "$LINE" == "FF10EF" ]]
-        then
-           mpc play 4
-        fi
-        # Play #5
-        if [[ "$LINE" == "FF38C7" ]]
-        then
-           mpc play 5
-        fi
-        # Play #6
-        if [[ "$LINE" == "FF5AA5" ]]
-        then
-           mpc play 6
-        fi
-        # Play #7
-        if [[ "$LINE" == "FF42BD" ]]
-        then
-           mpc play 7
-        fi
-        # Play #8
-        if [[ "$LINE" == "FF4AB5" ]]
-        then
-           mpc play 8
-        fi
-        # Play #9
-        if [[ "$LINE" == "FF52AD" ]]
-        then
-           mpc play 9
-        fi
-        # Play #0
-        if [[ "$LINE" == "FF6897" ]]
-        then
-           mpc play 10
         fi
 done
