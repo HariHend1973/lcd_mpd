a6cb6f5b1eacfd1edba8faadec541f2a9bf300a4
Hari Hendaryanto <hari.hendaryanto@gmail.com>
2018-02-02 09:20:45 +0700

    Add comment

--- lcd_mpd.sh
+++ lcd_mpd.sh
@@ -3,11 +3,14 @@ while true
 do
   echo "$(3ginfo 2> /dev/null)" > /tmp/3ginfotmp;
   sleep 3;
-  echo -n "$(/root/mpdlcd/wifi.sh)" > /root/mpdlcd/lcd_wifi.txt;  
+  # get wifi signal stat
+  echo -n "$(/root/mpdlcd/wifi.sh)" > /root/mpdlcd/lcd_wifi.txt;
+  # get tx/rx usage stat
   echo -n "$(/root/mpdlcd/txrx.sh)" > /root/mpdlcd/lcd_b2.txt;
+  # get modem signal stat
   echo -n "$(/root/mpdlcd/ss2.sh)" > /root/mpdlcd/lcd_b3.txt;
+  # get sbc temperature stat (gigi sbc)
   echo -n "$(/root/mpdlcd/temp.sh)" > /root/mpdlcd/lcd_b4.txt;
-  #echo -n "$(/root/mpdlcd/ss.sh)" > /root/mpdlcd/lcd_ss.txt;
 done &
 /etc/init.d/mpd start >/dev/null 2>&1 &
 cd /root/mpdlcd/
