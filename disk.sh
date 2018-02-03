3cb94b4b28e335b662942437b8ef640ea665d334
Hari Hendaryanto <hari.hendaryanto@gmail.com>
2018-02-03 17:13:10 +0700

    disk.sh

---
+++ disk.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+size=$(df -h | grep "/dev/root" | awk '{print $2}')
+used1=$(df -h | grep "/dev/root" | awk '{print $3}')
+avail=$(df -h | grep "/dev/root" | awk '{print $4}')
+
+mem=$(free | grep Mem)
+total=$(free | grep Mem | awk '{print $2}')
+used2=$(free | grep Mem | awk '{print $3}')
+free=$(free | grep Mem | awk '{print $4}')
+shared=$(free | grep Mem | awk '{print $5}')
+buffer=$(free | grep Mem | awk '{print $6}')
+cached=$(free | grep Mem | awk '{print $7}')
+
+total=$((total/1024))
+used3=$((used/1024))
+free=$((free/1024))
+shared=$((shared/1024))
+buffer=$((buffer/1024))
+cached=$((cached/1024))
+
+echo "S:$size U:$used1"
+echo "available: $avail"
+echo "T:"$total"M U:"$used"M F:"$free"M"
+echo "B:"$buffer"M C:"$cached"M S:"$shared"M"
