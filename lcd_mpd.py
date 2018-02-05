a86cac4d070a51213ea8af0a4fdd7b2afb41d036
Hari Hendaryanto <hari.hendaryanto@gmail.com>
2018-02-05 07:11:58 +0700

    fix clock when mpd stopped

--- lcd_mpd.py
+++ lcd_mpd.py
@@ -262,36 +262,36 @@ def main():
                 mpd_head_get()
                 ud_get(b2)
                 ss_get(b3_list,wifi)
-                temp_time_get(b4)
                 mpc_get()
                 for h in range(20):
+				        temp_time_get(b4)
                         time.sleep(0.1)
                         if len(station) != 0:
                                 break
 
                 mpd_head_get()
                 disk_get(disk_list)
-                temp_time_get(b4)
                 mpc_get()
                 for h in range(20):
+                        temp_time_get(b4)
                         time.sleep(0.1)
                         if len(station) != 0:
                                 break
 
                 mpd_head_get()
                 mem_get(disk_list)
-                temp_time_get(b4)
                 mpc_get()
                 for h in range(20):
+                        temp_time_get(b4)
                         time.sleep(0.1)
                         if len(station) != 0:
                                 break
 
                 mpd_head_get()
                 proc_get(disk_list)
-                temp_time_get(b4)
                 mpc_get()
                 for h in range(20):
+                        temp_time_get(b4)
                         time.sleep(0.1)
                         if len(station) != 0:
                                 break
