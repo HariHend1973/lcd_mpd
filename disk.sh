ba2ebe09a81cfe75593dcc2ae4551b810d94f033
Hari Hendaryanto <hari.hendaryanto@gmail.com>
2018-02-05 20:56:08 +0700

    Merge ss2.sh to disk.sh

--- disk.sh
+++ disk.sh
@@ -23,9 +23,54 @@ usr=$(top -bn 1 | head -n 5 | grep "CPU:" | awk '{print $3, $4}')
 nic=$(top -bn 1 | head -n 5 | grep "CPU:" | awk '{print $7, $8}')
 irq=$(top -bn 1 | head -n 5 | grep "CPU:" | awk '{print $13, $14}')
 
+# 3ginfo
+ss="$(grep strength /tmp/3ginfotmp | awk '{print $3}' 2> /dev/null)"
+# wifi
+wifi="$(cat /proc/net/wireless | awk 'NR==3 {print $4}' | sed 's/\.//')"
+# tx/rx
+txrx=$(grep Rec /tmp/3ginfotmp | cut -d':' -f 2 | sed 's/ //g' | sed 's/i//g' 2> /dev/null)
+tx=$(printf $txrx | cut -d"/" -f 1)
+rx=$(printf $txrx | cut -d"/" -f 2)
+txB=$(printf $tx | sed 's/[0-9\.]*//g')
+rxB=$(printf $rx | sed 's/[0-9\.]*//g')
+signal=${ss%?};
+
 echo "S:$size U:$used1"
 echo "available: $avail"
 echo "T:"$total"M U:"$used2"M S:"$shared"M"
 echo "B:"$buffer"M C:"$cached"M F:"$free"M"
 echo $cpu" "$usr
 echo $nic" "$irq
+if [ $signal -eq 0 ]
+   then
+        #echo -n "4G Signal: $(echo $'\xDB\xDB\xDB\xDB\xDB') $ss"
+        echo "signal0"
+fi
+if [ $signal -gt 0 ] && [ $signal -lt 21 ]
+   then
+        #echo -n "4G Signal: $(echo $'\xFF\xDB\xDB\xDB\xDB') $ss"
+        echo "signal20"
+fi
+if [ $signal -gt 20 ] && [ $signal -lt 41 ]
+   then
+        #echo -n "4G Signal: $(echo $'\xFF\xFF\xDB\xDB\xDB') $ss"
+        echo "signal40"
+fi
+if [ $signal -gt 40 ] && [ $signal -lt 61 ]
+   then
+        #echo -n "4G Signal: $(echo $'\xFF\xFF\xFF\xDB\xDB') $ss"
+        echo "signal60"
+fi
+if [ $signal -gt 60 ] && [ $signal -lt 81 ]
+   then
+        #echo -n "4G Signal: $(echo $'\xFF\xFF\xFF\xFF\xDB') $ss"
+        echo "signal80"
+fi
+if [ $signal -gt 80 ] && [ $signal -lt 101 ]
+   then
+        #echo -n "4G Signal: $(echo $'\xFF\xFF\xFF\xFF\xFF') $ss"
+        echo "signal100"
+fi
+echo " $ss"
+echo "${wifi}dBm"
+echo "$tx/$rx"
