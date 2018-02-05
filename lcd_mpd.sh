1c1d31e650025e3713ba7d8ebe1f7e66195a0a2f
Hari Hendaryanto <hari.hendaryanto@gmail.com>
2018-02-05 21:08:28 +0700

    Fix typo

--- lcd_mpd.sh
+++ lcd_mpd.sh
@@ -2,8 +2,9 @@
 while true
 do
   echo "$(3ginfo 2> /dev/null)" > /tmp/3ginfotmp;
-  sleep 1; -n "$(/root/mpdlcd/temp.sh)" > /root/mpdlcd/lcd_b4.txt;
-  echo -n "$(/root/mpdlcd/disk.sh)" > /root/mpdlcd/lcd_disk.txt;
+  sleep 1;
+  echo -n "$(/root/mpdlcd/temp.sh)" > /root/mpdlcd/lcd_b4.txt;
+  echo "$(/root/mpdlcd/disk.sh)" > /root/mpdlcd/lcd_disk.txt;
 done &
 /etc/init.d/mpd start >/dev/null 2>&1 &
 cd /root/mpdlcd/
